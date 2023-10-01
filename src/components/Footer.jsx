de57a7c43731580b9c0a3a54fb0f6c09579acda0
nastacosta <go.acosta3@gmail.com>
2023-09-30 23:09:00 -0300

    Fix footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 function Footer() {
   return (
-    <section className="w-full h-[10rem] flex items-center justify-center gap-56">
+    <section className="relative w-full h-[10rem] flex items-center justify-center gap-[37rem]">
       <p className="text-neutral-400 w-64">
         © Copyright All Rights Reserved Sushi Boom London, 2023
       </p>
-      <img className="h-[80%] py-4" src="/img/SB-Logo-F.png" alt="logo" />
+      <img
+        className="absolute h-[80%] py-4"
+        src="/img/SB-Logo-F.png"
+        alt="logo"
+      />
       <div className="text-neutral-500 flex gap-16">
         <button className="underline">Privacy Policy</button>
         <button className="underline">Terms of Service</button>
